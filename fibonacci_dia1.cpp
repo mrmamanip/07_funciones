0de177bf2fd76472301026bcf64982eab5c50edb
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-05-28 20:36:53 -0500

    Actualización: Día 1 - Serie y suma de serie Fibonacci - Orden y posibilidad de generar de nuevo otra serie

--- fibonacci_dia1.cpp
+++ fibonacci_dia1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
+
 void generarFibonacci(int n) {
-    int a, b, c, suma;
-	a = 0;
-	b = 1;
-	suma = 0;
-    cout << "Serie de Fibonacci con " << n << " términos:" << endl;
+    int a = 0, b = 1, c, suma = 0;
+    
+    cout << "Serie de Fibonacci con " << n << " terminos:" << endl;
+    
     for (int i = 0; i < n; i++) {
         if (i <= 1) {
             c = i;
@@ -17,16 +17,29 @@ void generarFibonacci(int n) {
         cout << c << " ";
         suma += c;
     }
-    cout << endl << "La suma de los terminos es: " << suma << endl;
+    cout << "\nLa suma de los terminos es: " << suma << endl;
 }
+
 int main() {
-    int terminos;
-    cout << "Ingrese la cantidad de términos de Fibonacci que desea generar: ";
-    cin >> terminos;
-    if (terminos <= 0) {
-        cout << "Por favor ingrese un número positivo." << endl;
-    } else {
-        generarFibonacci(terminos);
-    }
+	char option;
+	
+	do {
+		int terminos;
+		
+		cout << "Ingrese la cantidad de terminos de Fibonacci que desea generar: ";
+   		cin >> terminos;
+    
+    	if (terminos <= 0) {
+        	cout << "Por favor ingrese un numero positivo." << endl;
+    	} else {
+        	generarFibonacci(terminos);
+    	}
+    	
+    	cout << "Desea generar otra serie y suma? (s/n): ";
+    	cin >> option;
+	} while (option == 's' || option == 'S');
+	
+	cout<<"Programa finalizado.";
+    
     return 0;
 }
