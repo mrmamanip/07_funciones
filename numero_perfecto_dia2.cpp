392bc400acbff34ab3b0b7e72c6582a2b29b2b2c
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-05-29 21:37:55 -0500

    Actualización - Día 2 - Corroborar Número Perfecto - Restricción a números negativos y cero

--- numero_perfecto_dia2.cpp
+++ numero_perfecto_dia2.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
+
 int SumaDeDivisores(int n) {
 	int sumdiv = 0;
+	
 	for (int i = 1; i < n ; i++ ) {
 		if (n % i == 0) {
 			sumdiv = sumdiv + i;
 		}
 	}
+	
 	if (sumdiv == n) {
         return 1;
     } else {
@@ -15,11 +18,20 @@ int SumaDeDivisores(int n) {
 }
 
 int main(){
+	
 	int num;
 
 	cout << "Ingrese el numero que se desea corroborar si es perfecto: ";
 	cin >> num;
 
+	if ( num < 0 ) {
+		cout<<"Un numero perfecto no puede ser negativo.";
+		return 0;
+	} else if ( num == 0 ) {
+		cout << "0 no es un numero perfecto.";
+		return 0;
+	}
+	
 	if (SumaDeDivisores(num)) {
         cout << num << " es un numero perfecto.";
     } else {
