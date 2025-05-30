de8c22bd6ca1f1c12efaf490a3c36d17edf13645
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-05-30 18:30:16 -0500

    Actualización: Día 3 - Números Amigos - Posibilidad de voler a usar el programa

--- numeros_amigos_dia3.cpp
+++ numeros_amigos_dia3.cpp
@@ -24,26 +24,41 @@ int sonAmigos(int a, int b) {
 
 int main() {
     int num1, num2;
+    char option;
+    do {
+    	cout << "Ingrese dos numeros enteros positivos:" << endl;
+    	cout << "Ingrese el primer numero: ";
+    	cin >> num1;
+		cout << "Ingrese el segundo numero: "; 
+		cin >> num2;
     
-    cout << "Ingrese dos numeros enteros positivos:" << endl;
-    cout << "Ingrese el primer numero: ";
-    cin >> num1;
-	cout << "Ingrese el segundo numero: "; 
-	cin >> num2;
-    
-    if ( num1 < 0 || num2 < 0 ) {
-    	cout << "Ningun numero debe ser negativo.";
-    	return 0;
-	} else if ( num1 == 0 || num2 == 0 ) {
-		cout << "Ningun numero debe ser 0.";
-		return 0;
-	}
-    
-    if (sonAmigos(num1, num2)) {
-        cout << num1 << " y " << num2 << " son numeros amigos." << endl;
-    } else {
-        cout << num1 << " y " << num2 << " no son numeros amigos." << endl;
-    }
-    
+    	if ( num1 < 0 || num2 < 0 ) {
+    		cout << "Ningun numero debe ser negativo." << endl;
+
+		} else if ( num1 == 0 || num2 == 0 ) {
+			cout << "Ningun numero debe ser 0." << endl;
+
+		} else {
+			
+			if (sonAmigos(num1, num2)) {
+    		    cout << num1 << " y " << num2 << " son numeros amigos." << endl;
+    		} else {
+    	    cout << num1 << " y " << num2 << " no son numeros amigos." << endl;
+    		}
+		}
+    	
+    	cout << "Desea volver a corroborar si dos numeros son amigos? (s/n):" << endl;
+    	do {
+    		cin >> option;
+    		
+    		if (option != 's' && option != 'S' && option != 'n' && option != 'N'){
+    			cout << "Ingrese SOLO letras (s/n): ";
+			}
+		} while (option != 's' && option != 'S' && option != 'n' && option != 'N');
+		
+	} while ( option == 's' || option == 'S' );
+	
+	cout << "Programa finalizado.";
+	
     return 0;
 }
