07bd437f05288e4dbe6015a2fe4688a1a0ecdfc1
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-02 18:45:45 -0500

    Actualización: Día 5 - Hallar MCD y MCM de dos números - Corrección a resultado MCD y MCM negativos

--- mcm_mcd_dia5.cpp
+++ mcm_mcd_dia5.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 using namespace std;
 
+int valorAbsoluto(int numero) {
+    if (numero < 0) {
+        return -numero;
+    }
+    return numero;
+}
+
 int calcularMCD(int a, int b) {
     while (b != 0) {
         int temp = b;
         b = a % b;
         a = temp;
     }
-    return a;
+    return valorAbsoluto(a);
 }
 
 int calcularMCM(int a, int b) {
-    return (a * b) / calcularMCD(a, b);
+    return valorAbsoluto((a * b) / calcularMCD(a, b));
 }
 
 int main() {
     int num1, num2;
     
-    cout << "Ingrese el primer número: ";
+    cout << "Ingrese el primer numero: ";
     cin >> num1;
     
-    cout << "Ingrese el segundo número: ";
+    cout << "Ingrese el segundo numero: ";
     cin >> num2;
     
-    cout << "El Máximo Común Divisor (MCD) de " << num1 << " y " << num2 
+    cout << "El Maximo Comun Divisor (MCD) de " << num1 << " y " << num2 
          << " es: " << calcularMCD(num1, num2) << endl;
     
-    cout << "El Mínimo Común Múltiplo (MCM) de " << num1 << " y " << num2 
+    cout << "El Mínimo Común Multiplo (MCM) de " << num1 << " y " << num2 
          << " es: " << calcularMCM(num1, num2) << endl;
     
     return 0;
