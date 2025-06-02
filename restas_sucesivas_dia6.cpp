fedf083e81860194ff55545eca58fc8a62005222
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-02 18:27:44 -0500

    Actualización: Día 6 - Hallar cociente y resto mediante restas sucesivas - Posibilidad de volver a usar el programa

--- restas_sucesivas_dia6.cpp
+++ restas_sucesivas_dia6.cpp
@@ -22,23 +22,36 @@ void divisionPorRestas(int num1, int num2, int &cociente, int &resto) {
 
 int main() {
     int num1, num2, cociente, resto;
+	char option;
+	do { 
+    	do {
+			cout << "Ingrese el primer numero entero positivo: ";
+    		cin >> num1;
     
-    do {
-		cout << "Ingrese el primer numero entero positivo: ";
-    	cin >> num1;
+    		cout << "Ingrese el segundo numero entero positivo: ";
+    		cin >> num2;
+   	 
+    		if (num1 <= 0 || num2 <= 0) {
+        		cout << "Error: ambos numeros deben ser positivos." << endl;
+    		}
+    	} while (num1 <= 0 || num2 <= 0);
     
-    	cout << "Ingrese el segundo numero entero positivo: ";
-    	cin >> num2;
+    	divisionPorRestas(num1, num2, cociente, resto);
     
-    	if (num1 <= 0 || num2 <= 0) {
-        	cout << "Error: ambos numeros deben ser positivos." << endl;
-    	}
-    } while (num1 <= 0 || num2 <= 0);
+    	cout << "Cociente: " << cociente << endl;
+    	cout << "Resto: " << resto << endl;
+    	
+    	cout << "Desea volver hallar el cociente y resto de otros numeros? (s/n): ";
+    	
+    	do {
+    		cin >> option;
+    		
+    		if (option != 's' && option != 'S' && option != 'n' && option != 'N'){
+    			cout << "Ingrese SOLO letras (s/n): ";
+			}
+		} while (option != 's' && option != 'S' && option != 'n' && option != 'N');  
+    } while ( option == 's' || option == 'S' );
     
-    divisionPorRestas(num1, num2, cociente, resto);
-    
-    cout << "Cociente: " << cociente << endl;
-    cout << "Resto: " << resto << endl;
-    
+    cout << "Programa finalizado.";
     return 0;
 }
