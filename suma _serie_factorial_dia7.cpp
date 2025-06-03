9250ed204869c4e5e5d6150a88d57f5a0de64e81
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-02 19:43:32 -0500

    Actualización: Día 7 - Hallar la suma de n términos de la serie factorial - Posibilidad de volver a usar el programa

--- suma _serie_factorial_dia7.cpp
+++ suma _serie_factorial_dia7.cpp
@@ -19,16 +19,30 @@ double calcularSuma(double n) {
 
 int main() {
     int n;
+    char option;
     do {
-		cout << "Ingrese el valor de n: ";
-    	cin >> n;
+		do {
+			cout << "Ingrese el valor de n: ";
+    		cin >> n;
     
-    	if(n < 1) {
-    	    cout << "Error: n debe ser un entero positivo." << endl;
-    	}
-	} while (n < 1);
+    		if(n < 1) {
+    		    cout << "Error: n debe ser un entero positivo." << endl;
+    		}
+		} while (n < 1);
 	
-    cout << "El resultado de la suma es: " << calcularSuma(n) << endl;
+    	cout << "El resultado de la suma es: " << calcularSuma(n) << endl;
+    	
+    	cout << "Desea volver a calcular la suma de n terminos de la serie? (s/n): ";
+    	
+    	do {
+    		cin >> option;
+    		
+    		if (option != 's' && option != 'S' && option != 'n' && option != 'N'){
+    			cout << "Ingrese SOLO letras (s/n): ";
+			}
+		} while (option != 's' && option != 'S' && option != 'n' && option != 'N');    	
+    } while ( option == 's' || option == 'S' );
     
+    cout << "Programa finalizado.";
     return 0;
 }
